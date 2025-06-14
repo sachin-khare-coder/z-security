93b279d9b23e38a77c634f6ed3f3e8ccf308f8f8
sachin <sachin@gmail.com>
2025-06-14 21:03:43 +0530

    check age

---
+++ check.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class check {
+    public static void main(String[] args) {
+        int a;
+        Scanner sc = new Scanner (System.in);
+         a = sc.nextInt();
+         if (a>=18){
+            System.out.print("passed");
+         }
+        
+    }
+}
